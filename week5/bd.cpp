0cdb08bfc812dd7b605e302b1690d2a22768ce56
alexgovorov <alexgovorov@yandex-team.ru>
2021-10-15 22:10:00 +0300

    upd: week5 project

--- week5/bd.cpp
+++ week5/bd.cpp
@@ -1,36 +1,118 @@
 
 #include "bd.hpp"
+#include <fmt/core.h>
+#include <iomanip>
 
+Date::Date(int yy, int mm, int dd) :
+    y(yy), m(mm), d(dd) {
+  if (m < 1 || m > 12)
+    throw InvalidDateInput("Month value is invalid", m);
+  if (d < 1 || d > 31)
+    throw InvalidDateInput("Day value is invalid", d);
+}
 
 int Date::GetYear() const {
-
+  return y;
 }
 int Date::GetMonth() const {
-
+  return m;
 }
 int Date::GetDay() const {
-
+  return d;
 }
-void Date::FromStr(std::string s) {
-  
+
+void Parser::_process_args(ParseResult& res, std::istringstream& inp) {
+  int pos = inp.tellg();
+  _process_date(res, inp);
+  assert_space_after_date(inp, pos);
+  _process_event(res, inp);
+}
+void Parser::_process_command(ParseResult& res, std::istringstream& inp) {
+  inp >> res.command;
+  if (res.command == "")
+    return;
+  if (allowed_commands.count(res.command) == 0)
+    throw UnknownCommand(res.command);
 }
 
 Parser::ParseResult Parser::process(std::string s) {
   std::istringstream inp{s};
   ParseResult res;
-  inp >> res.command;
   _process_command(res, inp);
+  if (res.command == kPrint)
+    return res;
+  else if (res.command == kDel) {
+    _process_date(res, inp);
+    if (check_space_after_date(inp))
+      _process_event(res, inp);
+  } else {
+    _process_args(res, inp);
+  }
   return res;
 }
-void Parser::_process_args(ParseResult& res, std::istringstream& inp) {
-  // inp >> res.date;
+
+void Parser::throw_wrong_format(std::istringstream& inp, int pos) {
+  std::string what;
+  inp.clear();
+  inp.seekg(pos);
+  inp >> what;
+  throw WrongFormat(what);
 }
-void Parser::_process_command(ParseResult& res, std::istringstream& inp) {
-  if (allowed_commands.count(res.command) == 0)
-    throw std::invalid_argument(res.command);
-  if (res.command == kPrint)
-    return;
-  _process_args(res, inp);
+
+int Parser::get_int_before_sep(std::istringstream& inp, char sep, int pos) {
+  int i;
+  char _sep;
+  inp >> i;
+  inp >> _sep;
+  if (_sep != sep)
+    throw_wrong_format(inp, pos);
+  return i;
+}
+
+void Parser::assert_input_empty(std::istringstream& inp, int pos) {
+  std::string s;
+  inp >> s;
+  if (!inp.fail()) {
+    throw_wrong_format(inp, pos);
+  }
+}
+
+bool Parser::check_space_after_date(std::istringstream& inp) {
+  char c;
+  inp.get(c);
+  return c == ' ';
+}
+
+void Parser::assert_space_after_date(std::istringstream& inp, int pos) {
+  if (!check_space_after_date(inp))
+    throw_wrong_format(inp, pos);
+}
+
+void Parser::_process_date(Parser::ParseResult& res, std::istringstream& inp) {
+  int y, m, d;
+  std::string what;
+  y = m = d = 0;
+  auto pos = inp.tellg();
+  y = get_int_before_sep(inp, kDateSep, pos);
+  m = get_int_before_sep(inp, kDateSep, pos);
+  inp >> d;
+  if (inp.fail()) {
+    throw_wrong_format(inp, pos);
+  }
+  try {
+    Date date(y, m, d);
+    res.date = date;
+  } catch (std::invalid_argument) {
+    std::cout << y << ' ' << m << ' ' << d << '\n';
+    throw WrongFormat("Wrong date format");
+  }
+}
+
+void Parser::_process_event(Parser::ParseResult& res, std::istringstream& inp) {
+  std::string event;
+  int pos = inp.tellg();
+  inp >> res.event;
+  assert_input_empty(inp, pos);
 }
 
 void Database::AddEvent(const Date& date, const std::string& event) {
@@ -40,6 +122,8 @@ void Database::AddEvent(const Date& date, const std::string& event) {
 bool Database::DeleteEvent(const Date& date, const std::string& event) {
   if (data.count(date) == 0)
     return false;
+  data[date].erase(event);
+  return true;
 }
 
 int Database::DeleteDate(const Date& date) {
@@ -70,27 +154,45 @@ bool operator<(const Date& lhs, const Date& rhs) {
   if (ld != rd) return ld < rd;
   return false;
 }
-std::istream& operator>>(std::istream& inp, Date& date) {
-  return inp;
-}
 
-std::ostream& operator<<(std::ostream& out, Parser::ParseResult& res) {
-  out << res.command << " " /* << res.date << " " */<< res.event;
+std::ostream& operator<<(std::ostream& out, Date& date) {
+  out << date.GetYear() << '-' << date.GetMonth() << '-' << date.GetDay();
   return out;
 }
 
+std::ostream& operator<<(std::ostream& out, Parser::ParseResult& res) {
+  out << res.command << " " << res.date << " " << res.event;
+  return out;
+}
+
+std::string RunCommand(Database db, Parser::ParseResult com) {
+  if (com.command == Parser::kAdd) {
+
+  } else if (com.command == Parser::kDel) {
+
+  } else if (com.command == Parser::kFind) {
+
+  } else if (com.command == Parser::kPrint) {
+
+  }
+}
 
 int main() {
   Database db;
   std::string command;
+  int i;
   while (getline(std::cin, command)) {
     try {
       auto res = Parser().process(command);
       std::cout << res << '\n';
-    } catch (std::invalid_argument &e) {
+      std::cout << RunCommand(db, res);
+    } catch (UnknownCommand &e) {
       std::cout << "Unknown command: " << e.what() << '\n';
+    } catch (WrongFormat &e) {
+      std::cout << "Wrong date format: " << e.what() << '\n';
+    } catch (InvalidDateInput &e) {
+      std::cout << e.what() << '\n';
     }
   }
-
   return 0;
 }
--- week5/bd.hpp
+++ week5/bd.hpp
@@ -7,20 +7,36 @@
 #include <vector>
 #include <stdexcept>
 
-using Events = std::unordered_set<std::string>;
+class UnknownCommand : public std::runtime_error {
+  public:
+    UnknownCommand(std::string s) : std::runtime_error::runtime_error(s) {}
+};
+class WrongFormat : public std::runtime_error {
+  public:
+    WrongFormat(std::string s) : std::runtime_error::runtime_error(s) {}
+};
+class InvalidDateInput : public std::exception {
+  public:
+    InvalidDateInput(std::string s, int month_or_day): _what(s), _month_or_day(month_or_day) {}
+    std::string what() {
+      return _what + ": " + std::to_string(_month_or_day);
+    }
+  private:
+    std::string _what;
+    int _month_or_day;
+};
 
-const std::string kAdd = "ADD";
-const std::string kDel = "DELETE";
-const std::string kFind = "FIND";
-const std::string kPrint = "PRINT";
+using Events = std::unordered_set<std::string>;
+const char kDateSep = '-';
 
 class Date {
   public:
+    Date() = default;
+    Date(int, int, int);
     int GetYear() const;
     int GetMonth() const;
     int GetDay() const;
   private:
-    void FromStr(std::string s);
     int y = 0;
     int m = 1;
     int d = 1;
@@ -28,6 +44,10 @@ class Date {
 
 class Parser {
   public:
+    static const std::string kAdd = "ADD";
+    static const std::string kDel = "DELETE";
+    static const std::string kFind = "FIND";
+    static const std::string kPrint = "PRINT";
     struct ParseResult {
       std::string command;
       Date date;
@@ -37,7 +57,14 @@ class Parser {
   private:
     void _process_command(ParseResult& res, std::istringstream& inp);
     void _process_args(ParseResult& res, std::istringstream& inp);
-    std::istringstream inp;
+    void _process_date(Parser::ParseResult& res, std::istringstream& inp);
+    void _process_event(Parser::ParseResult& res, std::istringstream& inp);
+    void throw_wrong_format(std::istringstream& inp, int pos);
+    int get_int_before_sep(std::istringstream& inp, char sep, int pos);
+    bool check_space_after_date(std::istringstream& inp);
+    void assert_input_empty(std::istringstream& inp, int pos);
+    void assert_space_after_date(std::istringstream& inp, int pos);
+
     std::unordered_set<std::string> allowed_commands{kAdd, kDel,
                                                      kPrint, kFind};
 };
@@ -54,5 +81,5 @@ class Database {
 };
 
 bool operator<(const Date& lhs, const Date& rhs);
-std::istream& operator>>(std::istream& inp, Date& date);
+std::ostream& operator<<(std::ostream& out, Date& date);
 std::ostream& operator<<(std::ostream& out, Parser::ParseResult& res);
